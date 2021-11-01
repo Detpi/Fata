84d73f9dad9cd6ec46112f3ecd80c60344e8d381
hei-ui <hr705885@gmail.com>
2021-11-01 14:14:40 -0500

    first commit

---
+++ Reto3/src/main/java/com/advs/WebApp/ContadorClientes.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.advs.WebApp;
+
+import Model.Clientes;
+
+/**
+ *
+ * @author TATIANA
+ */
+public class ContadorClientes {
+    private Long total;
+     private Clientes client;
+
+    public ContadorClientes(Long total, Clientes client) {
+        this.total = total;
+        this.client = client;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Clientes getClient() {
+        return client;
+    }
+
+    public void setClient(Clientes client) {
+        this.client = client;
+    }
+     
+
+
+   
+}
